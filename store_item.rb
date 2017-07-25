8f4f13956e11b5b83c31c1596cb2cd4f79dd71e7
alianam <aliana@betterment.com>
2017-07-25 19:44:54 -0400

    Change initialize to accepts a hash

--- store_item.rb
+++ store_item.rb
@@ -2,10 +2,10 @@ class Paint
   attr_reader :color, :price, :series
   attr_writer :color, :price, :series
 
-  def initialize(input_color, input_price, input_series)
-    @color = input_color
-    @price = input_price
-    @series = input_series
+  def initialize(input_options)
+    @color = input_options[:color]
+    @price = input_options[:price]
+    @series = input_options[:series]
   end 
 
   def print_info
@@ -14,22 +14,22 @@ class Paint
 end
 
 
-blue_paint = Paint.new("blue", 10, "b")
-yellow_paint = Paint.new("yellow", 15, "a")
-red_paint = Paint.new("red", 9, "c")
+blue_paint = Paint.new({color: "blue", price: 10, series: "b"})
+# yellow_paint = Paint.new("yellow", 15, "a")
+# red_paint = Paint.new("red", 9, "c")
 
 blue_paint.print_info
-yellow_paint.print_info
-red_paint.print_info
+# yellow_paint.print_info
+# red_paint.print_info
 
-p "Changing cost of blue paint..."
-blue_paint.price = 15
-blue_paint.print_info
+# p "Changing cost of blue paint..."
+# blue_paint.price = 15
+# blue_paint.print_info
 
-p "changing the series..."
-blue_paint.series = "D"
-blue_paint.print_info
+# p "changing the series..."
+# blue_paint.series = "D"
+# blue_paint.print_info
 
-p blue_paint.color
-p blue_paint.price
-p blue_paint.series
+# p blue_paint.color
+# p blue_paint.price
+# p blue_paint.series
