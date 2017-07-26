3a087952291a98b4bf80b3441e7570a6de48a82a
alianam <aliana@betterment.com>
2017-07-25 21:59:44 -0400

    Add method to fire all employees

--- manager.rb
+++ manager.rb
@@ -41,6 +41,12 @@ class Manager < Employee
     end
   end
 
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false
+    end
+  end
+
   def send_report
     puts "Sending email..."
     # use email sending library...
@@ -52,4 +58,6 @@ manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000
 # manager.print_info
 # manager.send_report
 
-manager.give_all_raises
+manager.fire_all_employees
+p employee1
+p employee2
